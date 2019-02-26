8408bbc75855360a75c76eb5a4c9b949e1c2b025
jiankai <lee_jk1996@hotmail.com>
2019-02-25 19:28:34 -0600

    npm start

--- webpack.config.js
+++ webpack.config.js
@@ -16,8 +16,7 @@ module.exports = {
             {enforce: "pre",test:/\.js$/,loader:"source-map-loader"}
         ]
     },
-    // externals: {
-    //     "react":"React",
-    //     "react-dom":"ReactDOM"
-    // }
+    devServer:{
+        publicPath: '/dist/',
+    },
 };
